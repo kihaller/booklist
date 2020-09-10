9220e1c0c8075e7839787f108a4d823d4e309b71
kihaller <67028582+kihaller@users.noreply.github.com>
2020-09-10 15:43:16 -0700

    Update main.js
    duplicate function, isbn validation function, mock data, book covers from API

--- main.js
+++ main.js
@@ -25,12 +25,14 @@ class Book {
 /* -------------
 Function Definitions
 ------------- */
+//render fucntion
 function render() {
   TABLE.innerHTML = "";
 
   const header = document.createElement("tr");
   header.innerHTML = `
         <tr>
+        <th>Cover</th>
           <th>Title</th>
           <th>Author</th>
           <th>ISBN</th>
@@ -42,6 +44,7 @@ function render() {
   books.forEach((book) => {
     const row = document.createElement("tr");
     row.innerHTML = `
+    <td><img src="http://covers.openlibrary.org/b/isbn/${book.isbn}-S.jpg" /></td>
     <td>${book.title}</td>
     <td>${book.author}</td>
     <td>${book.isbn}</td>
@@ -59,11 +62,24 @@ function render() {
   });
 }
 
-//event listener on trash icon
-function deleteBook(book) {
-  books = books.filter((item) => item !== book);
-  render();
-  localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(books));
+//validate function
+function validate(isbn) {
+  const isbnEntered = /\d{10,13}/;
+  const validISBN = isbnEntered.test(BOOKISBN.value);
+  return validISBN;
+}
+
+//check duplicate function
+function isDuplicate(book) {
+  const bookISBN = book.isbn;
+  if (books.filter((item) => item.isbn === book.isbn).length > 0) {
+    ALERT.style.display = "flex";
+    ALERT.innerHTML = "You already have this book in your list.";
+    $(ALERT).fadeIn(200);
+    $(ALERT).delay(1500).fadeOut(1000);
+    return true;
+  }
+  return false;
 }
 
 /* -------------
@@ -74,28 +90,52 @@ Event Listeners
 SUBMITBUTTON.addEventListener("click", (e) => {
   e.preventDefault();
   //check whether all three input fields have been filled in
-  if (BOOKTITLE.value && BOOKAUTHOR.value && BOOKISBN.value) {
+  if (
+    BOOKTITLE.value &&
+    BOOKAUTHOR.value &&
+    BOOKISBN.value &&
+    validate(BOOKISBN.value)
+  ) {
     //Create a new instance of the book class
     const book1 = new Book(BOOKTITLE.value, BOOKAUTHOR.value, BOOKISBN.value);
-    books.push(book1);
-    console.log(books);
-    render();
 
-    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(books));
+    if (!isDuplicate(book1)) {
+      books.push(book1);
+      console.log(books);
+      render();
+      ALERT.style.display = "flex";
+      ALERT.style.backgroundColor = "#32CD32";
+      ALERT.innerHTML = "The book was successfully added.";
+      $(ALERT).fadeIn(200);
+      $(ALERT).delay(1500).fadeOut(1000);
+      localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(books));
+    }
+  } else if (!BOOKTITLE.value || !BOOKAUTHOR.value || !BOOKISBN.value) {
+    ALERT.style.display = "flex";
+    ALERT.style.backgroundColor = "rgb(245, 84, 159);";
+    ALERT.innerHTML = "Please provide all the requested information";
+    $(ALERT).fadeIn(200);
+    $(ALERT).delay(4000).fadeOut(2000);
   } else {
     ALERT.style.display = "flex";
-    ALERT.innerHTML = "Please provide all the requested information.";
+    ALERT.style.backgroundColor = "rgb(245, 84, 159);";
+    ALERT.innerHTML = "Please ensure the ISBN you entered is correct.";
     $(ALERT).fadeIn(200);
     $(ALERT).delay(4000).fadeOut(2000);
   }
 });
 
 //event listener on trash icon
-document.querySelectorAll(".trash-icon").forEach((item) => {
-  item.addEventListener("click", () => {
-    item.parentNode.parentNode.style.display = "none";
-  });
-});
+function deleteBook(book) {
+  books = books.filter((item) => item !== book);
+  render();
+  localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(books));
+  ALERT.style.display = "flex";
+  ALERT.style.backgroundColor = "#DC143C";
+  ALERT.innerHTML = "The book was successfully removed.";
+  $(ALERT).fadeIn(200);
+  $(ALERT).delay(1500).fadeOut(1000);
+}
 
 /* ------------
 Execute on load
@@ -103,6 +143,20 @@ Execute on load
 
 window.onload = function () {
   books = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+
+  if (books === null) {
+    books = [
+      new Book(
+        "Harry Potter and the Deathly Hallows",
+        "J.K. Rowling",
+        9780545010221
+      ),
+      new Book("Five Feet Apart", "Rachael Lippincott", 9781534437333),
+      new Book("The Wonderful Wizard of Oz", "L. Frank Baum", 9780688166779),
+    ];
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(books));
+  }
+
   console.log(books);
   render();
 };
